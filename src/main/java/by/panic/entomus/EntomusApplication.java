6621fc9e17d41c74a7b22f1e9d71955057d134e0
Andrey <andrey1010102008@gmail.com>
2024-02-19 00:00:19 +0300

    v1

---
+++ src/main/java/by/panic/entomus/EntomusApplication.java
@@ -0,0 +1,13 @@
+package by.panic.entomus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntomusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntomusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/panic/entomus/EntomusApplicationTests.java
@@ -0,0 +1,13 @@
+package by.panic.entomus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntomusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
